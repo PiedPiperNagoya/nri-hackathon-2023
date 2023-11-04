10415db84f184a708c067b476b72fa090a1850d3
ree-rishun <ree0432@gmail.com>
2023-11-04 11:53:22 +0900

    feature: #1 新規登録画面のルーティング追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignupView from '../views/Auth/SignupView'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignupView,
   },
 ]
 
