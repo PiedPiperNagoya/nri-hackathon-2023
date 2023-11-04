773ef9e9997afbea8ec44393e1081a04394a1d27
ree-rishun <ree0432@gmail.com>
2023-11-04 11:52:18 +0900

    feature: #1 共通コンポーネントの作成

---
+++ src/components/BasicTitleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>
+    {{ title }}
+  </h1>
+</template>
+
+<script>
+  export default {
+    name: 'BasicTitleComponent',
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  h1 {
+    display: block;
+    width: 90%;
+    max-width: 400px;
+    height: 60px;
+    line-height: 60px;
+    margin: 0 auto;
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+  }
+</style>
---
+++ src/components/BottomButtonComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="bottom-button">
+    <button
+      @click="$emit('onclick')">
+      {{ content }}
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BottomButtonComponent',
+    props: {
+      content: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .bottom-button {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 200;
+    display: block;
+    width: 100%;
+    margin: 0;
+    padding: 20px 0;
+  }
+  button {
+    display: block;
+    width: 90%;
+    max-width: 400px;
+    height: 60px;
+    line-height: 60px;
+    margin: 0 auto;
+    border-radius: 5px;
+    background-color: #FF5700;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 20px;
+    border: none;
+    outline: none;
+  }
+</style>
